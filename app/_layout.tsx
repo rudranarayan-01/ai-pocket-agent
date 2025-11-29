6140c4f809079367576102abdfe19b0e2f6e9779
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-11-29 12:57:42 +0530

    clerk setting up

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,13 @@
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{headerShown:false}}>
-      <Stack.Screen name="index" />
-    </Stack>
+    <ClerkProvider tokenCache={tokenCache}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+      </Stack>
+    </ClerkProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,83 @@
 import Colors from "@/shred/Colors";
+import { useAuth } from "@clerk/clerk-expo";
+import { useCallback, useEffect } from "react";
 import { Dimensions, Image, Platform, Text, TouchableOpacity, View } from "react-native";
 
+// Clerk expo oAuth browser warm up
+import { useSSO } from '@clerk/clerk-expo';
+import * as AuthSession from 'expo-auth-session';
+import { useRouter } from "expo-router";
+import * as WebBrowser from 'expo-web-browser';
+
+export const useWarmUpBrowser = () => {
+  useEffect(() => {
+    if (Platform.OS !== 'android') return
+    void WebBrowser.warmUpAsync()
+    return () => {
+      // Cleanup: closes browser when component unmounts
+      void WebBrowser.coolDownAsync()
+    }
+  }, [])
+}
+
+// Handle any pending authentication sessions
+WebBrowser.maybeCompleteAuthSession()
+
+
 export default function Index() {
+
+  const { isSignedIn } = useAuth()
+  const router = useRouter();
+
+  useEffect (() => {
+    if (isSignedIn) {
+      console.log("User is signed in")
+    } else {
+      console.log("User is NOT signed in")
+    }
+  }, [isSignedIn]);
+
+
+  ///////////////// This code from Clerk OAuth Expo example ////////////////////////
+  useWarmUpBrowser()
+
+  // Use the `useSSO()` hook to access the `startSSOFlow()` method
+  const { startSSOFlow } = useSSO()
+
+  const onLoginPress = useCallback(async () => {
+    try {
+      // Start the authentication process by calling `startSSOFlow()`
+      const { createdSessionId, setActive, signIn, signUp } = await startSSOFlow({
+        strategy: 'oauth_google',
+        redirectUrl: AuthSession.makeRedirectUri(),
+      })
+      if (createdSessionId) {
+        setActive!({
+          session: createdSessionId,
+          navigate: async ({ session }) => {
+            if (session?.currentTask) {
+              console.log(session?.currentTask)
+              // router.push('/sign-in/tasks')
+              return
+            }
+
+            router.push('/')
+          },
+        })
+      } else {
+        // If there is no `createdSessionId`,
+        // there are missing requirements, such as MFA
+        // See https://clerk.com/docs/guides/development/custom-flows/authentication/oauth-connections#handle-missing-requirements
+      }
+    } catch (err) {
+      // See https://clerk.com/docs/guides/development/custom-flows/error-handling
+      // for more info on error handling
+      console.error(JSON.stringify(err, null, 2))
+    }
+  }, [])
+
+////////////////////////////////////////
+
   return (
     <View
       style={{
@@ -31,7 +107,7 @@ export default function Index() {
         }}>Your personal AI assistant on the go. Try it today. Completely free.</Text>
       </View>
 
-      <TouchableOpacity style={{width:"100%", marginTop:16, padding:15, backgroundColor:Colors.PRIMARY, borderRadius:12, justifyContent:"center", alignItems:"center"}}>
+      <TouchableOpacity onPress={onLoginPress} style={{width:"100%", marginTop:16, padding:15, backgroundColor:Colors.PRIMARY, borderRadius:12, justifyContent:"center", alignItems:"center"}}>
         <Text style={{color:Colors.WHITE, fontSize:24, textAlign:"center"}}>Get Started</Text>
       </TouchableOpacity>
 
