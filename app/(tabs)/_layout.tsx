2e56cc94aa08d6b40201a3fab2f128fe4f6d1067
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-11-30 20:27:31 +0530

    Tab done lets do for home screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router'
-import { HomeIcon } from 'lucide-react-native'
+import { Globe, History, HomeIcon, UserCircle } from 'lucide-react-native'
 import React from 'react'
 
 export default function TabLayout() {
@@ -8,9 +8,15 @@ export default function TabLayout() {
             <Tabs.Screen name='Home' options={{
                 tabBarIcon:({color,size}) => <HomeIcon size={size} color={color}/>
             }} />
-            <Tabs.Screen name='Explore' />
-            <Tabs.Screen name='History' />
-            <Tabs.Screen name='Profile' />
+            <Tabs.Screen name='Explore' options={{
+                tabBarIcon:({color,size}) => <Globe size={size} color={color}/>
+            }} />
+            <Tabs.Screen name='History' options={{
+                tabBarIcon:({color,size}) => <History size={size} color={color}/>
+            }} />
+            <Tabs.Screen name='Profile' options={{
+                tabBarIcon:({color,size}) => <UserCircle size={size} color={color}/>
+            }} />
         </Tabs>
     )
 }
