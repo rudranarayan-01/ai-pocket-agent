7123fd3c85e1b819f7746629ddc787f2aefa9396
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-12-01 08:49:37 +0530

    Designing home page

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { useNavigation } from 'expo-router';
+import React, { useEffect } from 'react';
+import { Text, View } from 'react-native';
 
 export default function Home() {
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        navigation.setOptions({headerTitle: () => (<Text style={{fontSize:18,fontWeight:'bold'}}>
+            Welcome to AI Pocket Agent
+        </Text>)});
+    })
     return (
         <View>
             <Text>Home</Text>
