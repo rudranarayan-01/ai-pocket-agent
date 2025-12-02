ef9218508b33cb97c80e53cb927811bef8717ad8
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-12-02 17:55:25 +0530

    Banner designing

--- components/Home/CreateAgentBanner.tsx
+++ components/Home/CreateAgentBanner.tsx
@@ -1,10 +1,14 @@
+import Colors from '@/shred/Colors'
 import React from 'react'
-import { Image, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
 
 export default function CreateAgentBanner() {
     return (
-        <View>
-            <Image source={require("../../assets/images/agents/agentGroup.png")} style={{width:200, height:120}} />
+        <View style={{ backgroundColor: Colors.PRIMARY, padding: 15, borderRadius: 10, display: "flex", flexDirection: "row", marginTop: 15, shadowColor: Colors.BLACK, shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.1, shadowRadius: 10, elevation: 6 }}>
+            <Image source={require("../../assets/images/agents/agentGroup.png")} style={{ width: 200, height: 120, resizeMode: "contain" }} />
+            <View style={{ flex: 1, justifyContent: "center", alignItems: "flex-start", paddingLeft: 15 }}>
+                <Text style={{ color: Colors.WHITE, fontSize: 20, fontWeight: "bold", flex: 1 }}>Create your own AI Agent</Text>
+            </View>
         </View>
     )
 }
