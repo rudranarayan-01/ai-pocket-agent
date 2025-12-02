b38cf802c731a4ef472974084a4f599e80f85056
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-12-02 20:05:16 +0530

    Home Screen Done

--- components/Home/NonFeaturedAgentCard.tsx
+++ components/Home/NonFeaturedAgentCard.tsx
@@ -9,12 +9,12 @@ type Props = {
 
 export default function NonFeaturedAgentCard({ agent }: Props) {
     return (
-        <View style={{ padding: 15, minHeight: 200, backgroundColor: Colors.WHITE, borderRadius: 10, shadowColor: Colors.BLACK, shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.1, shadowRadius: 10, elevation: 6, overflow: 'hidden' }}>
-            <View style={{ right: 0, bottom: 0, position: 'absolute' }}>
+        <View style={{ padding: 15, minHeight: 180, backgroundColor: Colors.WHITE, borderRadius: 10, shadowColor: Colors.BLACK, shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.1, shadowRadius: 10, elevation: 6, overflow: 'hidden' }}>
+            <View>
                 {/* @ts-ignore */}
-                <Image source={agent.image} style={{ width: 100, height: 100, resizeMode: 'contain', marginTop: 10 }} />
+                <Image source={agent.image} style={{ width: 80, height: 80, resizeMode: 'contain', marginTop: 10 }} />
             </View>
-            <View style={{ padding: 15, backgroundColor: Colors.WHITE, justifyContent: 'center', alignItems: 'center', marginBottom: 10, borderRadius: 10, }}>
+            <View style={{marginTop: 10}}>
                 <Text style={{ fontSize: 20, fontWeight: 'bold' }}>{agent.name}</Text>
                 <Text numberOfLines={2} style={{ color: Colors.GRAY, marginTop: 2 }}>{agent.desc}</Text>
             </View>
