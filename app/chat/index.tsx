bcb2631b9c59408c7c8fb8fac963c150639b23c7
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-12-04 17:35:31 +0530

    Implementing chat UI

--- app/chat/index.tsx
+++ app/chat/index.tsx
@@ -3,6 +3,15 @@ import { Plus } from 'lucide-react-native';
 import React, { useEffect } from 'react';
 import { Text, View } from 'react-native';
 
+const initialMessages = [
+    {
+        id: '1',
+        text: 'Hello! How can I assist you today?',
+        sender: 'agent',
+        timestamp: '2023-10-01T10:00:00Z',  
+    },
+]
+
 export default function ChatUI() {
     const navigation = useNavigation();
     const {agentName, agentPrompt, agentInitialText, agentId} = useLocalSearchParams();
